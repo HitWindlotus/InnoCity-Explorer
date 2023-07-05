309e8520e2d70cd5ffb1e9818ab21bdeff8ab05b
HitWindlotus <91593371+HitWindlotus@users.noreply.github.com>
2023-07-05 19:52:37 +0800

    Add files via upload

---
+++ InnoCity.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import random
+import pandas as pd
+import dataframe_image as dfi
+import datetime
+from pandas.plotting import table
+import matplotlib.pyplot as plt
+import matplotlib
+
+# 标题
+st.title('InnoCity Explorer')
+
+
+
+
